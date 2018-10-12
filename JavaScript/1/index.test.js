fa17aa02ecc99c2995a7ecc2973f6b3fc9cc7065
Samuel Cha <sunchipssam@gmail.com>
2018-10-12 16:57:46 -0700

    Removed arrow syntax per mocha doc

--- JavaScript/1/index.test.js
+++ JavaScript/1/index.test.js
@@ -4,8 +4,8 @@ const t = require('./');
 const s = require('./solution');
 const f = t() === undefined ? t : s;
 
-describe(`QUESTION 1: Is Unique`, _ => {
-    it(`should return true for simple strings`, done => {
+describe(`QUESTION 1: Is Unique`, function() {
+    it(`should return true for simple strings`, function(done) {
         assert.equal(f('asdf'), true);
 
         done();
