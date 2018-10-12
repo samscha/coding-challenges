782ee95c698d4651eb9735db36dfc93d3977b0a8
Samuel Cha <sunchipssam@gmail.com>
2018-10-12 16:55:58 -0700

    Added check for empty func

--- JavaScript/1/index.test.js
+++ JavaScript/1/index.test.js
@@ -2,9 +2,12 @@ const assert = require('assert');
 
 const t = require('./');
 const s = require('./solution');
+const f = t() === undefined ? t : s;
 
 describe(`QUESTION 1: Is Unique`, _ => {
-    it(`should return true for simple strings`, _ => {
-        assert.equal(t('asdf'), s('asdf'));
+    it(`should return true for simple strings`, done => {
+        assert.equal(f('asdf'), true);
+
+        done();
     });
 });
