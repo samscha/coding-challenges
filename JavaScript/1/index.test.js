28197f819e635bee0cb015d0a8f969c5a06c67c1
Samuel Cha <sunchipssam@gmail.com>
2018-10-13 12:24:25 -0700

    Fixed func check in test

--- JavaScript/1/index.test.js
+++ JavaScript/1/index.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const t = require('./');
 const s = require('./solution');
-const f = t() === undefined ? t : s;
+const f = t() !== undefined ? t : s;
 
 describe(`QUESTION 1: Is Unique`, function() {
     it(`should return true for simple strings`, function(done) {
