72364f916549c10c47ebe98cee4f98d30b43eecc
Samuel Cha <sunchipssam@gmail.com>
2018-10-19 07:36:41 -0700

    Fixed else and const bugs

--- JavaScript/2_Check-Permutation/solution.js
+++ JavaScript/2_Check-Permutation/solution.js
@@ -11,16 +11,16 @@ module.exports = (s1, s2) =>
 
         if (c1 === c2) continue;
 
-        const m1 = chars.get(c1);
-        const m2 = chars.get(c2);
+        let m1 = chars.get(c1);
+        let m2 = chars.get(c2);
 
         if (!m1) chars.set(c1, 1);
         else if (m1 === -1) chars.delete(c1);
-        else chars.set(c1, ++c1m);
+        else chars.set(c1, ++m1);
 
         if (!m2) chars.set(c2, -1);
         else if (m2 === 1) chars.delete(c2);
-        else chars.set(c2, --c2m);
+        else chars.set(c2, --m2);
     }
 
     return chars.size === 0;
